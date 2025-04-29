083265d27d925860b36b548b469301bbc6640968
ShaoJun18 <109631173+ShaoJun18@users.noreply.github.com>
2025-04-29 11:59:20 +0200

    ajout

---
+++ src/main/java/net/mcreator/dojutsumix/item/ItemOnetomoeindra.java
@@ -0,0 +1,60 @@
+
+package net.mcreator.dojutsumix.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import net.mcreator.dojutsumix.creativetab.TabDojutsu;
+import net.mcreator.dojutsumix.ElementsDojutsumix;
+
+@ElementsDojutsumix.ModElement.Tag
+public class ItemOnetomoeindra extends ElementsDojutsumix.ModElement {
+	@GameRegistry.ObjectHolder("dojutsumix:onetomoeindra")
+	public static final Item block = null;
+	public ItemOnetomoeindra(ElementsDojutsumix instance) {
+		super(instance, 9);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("dojutsumix:onetomoeindra", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 64;
+			setUnlocalizedName("onetomoeindra");
+			setRegistryName("onetomoeindra");
+			setCreativeTab(TabDojutsu.tab);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+	}
+}
